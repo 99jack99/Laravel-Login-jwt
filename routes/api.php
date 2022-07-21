8b4a401cc49ced67a1927d288244c7ab3039c668
jackson <jackson55adl@gmail.com>
2022-07-21 11:08:04 +0200

    adding profile endpoint

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,7 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/profile', [AuthController::class, 'profile'])->middleware('jwt.auth');
+
+Route::post('/logout', [AuthController::class, 'logout']);
